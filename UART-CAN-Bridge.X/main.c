b8cc30b9c63d772d06819f74762c2c0197aac3d1
Tom Paynter <thomas.paynter@student.curtin.edu.au>
2016-09-16 16:16:00 +0200

    Routine

--- UART-CAN-Bridge.X/main.c
+++ UART-CAN-Bridge.X/main.c
@@ -2,103 +2,98 @@
 #include <pic18f46k80.h>
 #include "../PIC18F46K80_Drivers/Config_Word/src/configword.h"
 #include "../PIC18F46K80_Drivers/UART/src/UART.h"
-#include "../PIC18F46K80_Drivers/CAN/src/CAN.h"
 
-
-unsigned char data1[8], data2[8];
-int main()
-{
-
-uartInitialisation();
-
-unsigned char incomingByte, plusCounter, incData[12], incDataCounter, endCounter, defaultSID = 1;
+unsigned char incomingByte, plusCounter, incData[16], incDataCounter, endCounter, defaultSID = 1;
 
 enum caseCounter_s {getData, ending1, ending2, done} caseCounter = done;
 
 
 
+void CAN_Transmit(unsigned char data[8], unsigned char SID, unsigned char DLC) {
+  int i;
+
+  uartTransmitByte('[');
+  uartTransmitByte(DLC);
+
+  for(i=0; i < DLC; i++){
+    uartTransmitByte(data[i]);
+  }
+
+  uartTransmitByte(SID);
+  uartTransmitByte(')');
+  uartTransmitByte(']');
+  uartTransmitByte('\r');
+
+  return;
+}
+
+void sendCANMessage(void) {
+  if(incDataCounter == (incData[0] + 4)) {
+    CAN_Transmit(&incData[1], 5, incData[0]);
+  }
+
+  else if(incDataCounter == (incData[0] + 5)) {
+    CAN_Transmit(&incData[1], incData[incDataCounter - 4], incData[0]);
+  }
+
+  else if(incDataCounter == (incData[0] + 6)) {
+      unsigned int tempID;
+      tempID = incData[incDataCounter - 5] * 256 & incData[incDataCounter - 4];
+      CAN_Transmit(&incData[1], tempID, incData[0]);
+  }
+
+  return;
+}
+
+unsigned char data1[8], data2[8];
+
+int main() {
+
+uartInitialisation();
+
+
 while(1) {
-    
+
     if (uartRecieveByte(&incomingByte)) {
-         
-        //uartTransmitByte(incomingByte);
-        
-        if (incomingByte == '+') {
-            plusCounter = plusCounter + 1;
-            
-            //if (plusCounter == 3 && ATIntroTimer == VALID) {
-              //  enterATMode();
-                //plusCounter = 0;
-        //    }
+
+        if ((incomingByte == '{') && (caseCounter == done)) {
+            caseCounter = getData;
+            incDataCounter = 0;
         }
-        
-        else {
-           // resetATIntroTimer();
-            
-            if ((incomingByte == '{') && (caseCounter != getData)) {
+
+        else if (caseCounter != done) {
+            incData[incDataCounter] = incomingByte;
+            incDataCounter = incDataCounter + 1;
+
+            if (incDataCounter > 15) {
+                caseCounter = done;
+            }
+        }
+
+        if ((incomingByte == ')') && (caseCounter != done)) {
+            caseCounter = ending1;
+        }
+
+        else if (caseCounter == ending1) {
+            if (incomingByte == 125) {
+              caseCounter = ending2;
+            }
+
+            else {
                 caseCounter = getData;
-                incDataCounter = 0;
-                uartTransmitByte('A');
             }
-            
-            else if ((caseCounter != done)) {
-                incData[incDataCounter] = incomingByte;
-                incDataCounter = incDataCounter + 1;
-                                uartTransmitByte('B');
-
-            }
-            
-            if ((incomingByte == ')') && (caseCounter != done)) {
-                caseCounter = ending1;
-                                uartTransmitByte('C');
-
-            }
-            
-            if ((incomingByte == '}') && (caseCounter == ending1)) {
-                caseCounter = ending2;
-                                uartTransmitByte('D');
-
-            }
-            else {
-                caseCounter == getData;
-            }
-            
-            if ((incomingByte == '\r') && (caseCounter == ending2)) {
-                caseCounter == done;
-                                uartTransmitByte('E');
-
-                
-                if(incData[0] == incDataCounter - 3 + '0') {
-                   //CAN_Transmit(&incData[1], defaultSID, incData[0]);
-                   }
-                
-                else if(incData[0] == incDataCounter - 4  + '0') {
-                    //CAN_Transmit(&incData[1], incData[incDataCounter + 1], incData[0]);
-                    }
-                
-                else if(incData[0] == incDataCounter - 5 + '0') {
-                    
-                    unsigned int tempID;
-                    
-                    tempID = incData[incDataCounter - 3] * 256 & incData[incDataCounter - 2 + '0'];
-                    
-                    //CAN_Transmit(&incData[1], tempID, incData[0]);
-                    
-                }
-                                
-                else {
-                    
-                }
-                
-            }
-            
-            else {
-                caseCounter == getData;
-            }
-            
         }
-        
+
+        else if (caseCounter == ending2) {
+            if (incomingByte == '\r') {
+              caseCounter = done;
+              sendCANMessage();
+            }
+
+        else {
+            caseCounter = getData;
+        }
+        }
     }
 }
 }
-
